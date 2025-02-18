dff1a787311d273c80286f161804a80bfc760b06
linyuchen <lin.yu.chen@hotmail.com>
2025-02-18 20:30:35 +0800

    feat: plugin repeat checker

--- src/plugins/check_repeat.py
+++ src/plugins/check_repeat.py
@@ -16,16 +16,22 @@ group_message_state = defaultdict(lambda : {'repeat_count': 0, 'last_message': '
 
 @on_message().handle()
 async def _(bot: Bot, event: GroupMessageEvent):
-    state = group_message_state.get(event.group_id)
-    if event.message.extract_plain_text().strip() == state['last_message']:
+    state = group_message_state[event.group_id]
+    current_message = event.message.extract_plain_text().strip()
+    if not current_message:
+        return
+    if current_message == state['last_message']:
         state['repeat_count'] += 1
-        repeat_max = random.randint(3, 10)
+        repeat_max = random.randint(3, 12)
         if state['repeat_count'] >= repeat_max:
-            ban_duration = repeat_max * 10
+            ban_duration = (repeat_max - 2)  * 60
             bot_info = await bot.get_group_member_info(group_id=event.group_id, user_id=int(bot.self_id))
             if bot_info.get('role') not in ['owner', 'admin']:
                 return
             await bot.set_group_ban(group_id=event.group_id, user_id=event.sender.user_id, duration=ban_duration)
-            await bot.send_group_msg(group_id=event.group_id, message=f'【{event.sender.nickname}】因为复读被禁言{ban_duration}分钟')
+            await bot.send_group_msg(group_id=event.group_id, message=f'【{event.sender.nickname}】因为复读被禁言{ban_duration}秒')
             state['repeat_count'] = 0
             state['last_message'] = ''
+    else:
+        state['repeat_count'] = 0
+        state['last_message'] = current_message
