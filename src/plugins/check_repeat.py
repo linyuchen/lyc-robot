424bc8bb84f50ccf15cd8e3099feb6da7a84ed97
linyuchen <lin.yu.chen@hotmail.com>
2025-02-18 17:51:08 +0800

    feat: plugin check repeat

--- src/plugins/check_repeat.py
+++ src/plugins/check_repeat.py
@@ -1,6 +1,8 @@
+import random
+from collections import defaultdict
+
 from nonebot.plugin import PluginMetadata
 from nonebot import on_message
-from nonebot_plugin_uninfo import Uninfo, SupportAdapter
 from nonebot_plugin_alconna import UniMsg
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 
@@ -10,8 +12,20 @@ __plugin_meta__ = PluginMetadata(
     usage="",
 )
 
-group_message_state = {}
+group_message_state = defaultdict(lambda : {'repeat_count': 0, 'last_message': ''})
 
 @on_message().handle()
 async def _(bot: Bot, event: GroupMessageEvent):
-    await bot.get_group_member_info(group_id=event.group_id, user_id=int(bot.self_id))
+    state = group_message_state.get(event.group_id)
+    if event.message.extract_plain_text().strip() == state['last_message']:
+        state['repeat_count'] += 1
+        repeat_max = random.randint(3, 10)
+        if state['repeat_count'] >= repeat_max:
+            ban_duration = repeat_max * 10
+            bot_info = await bot.get_group_member_info(group_id=event.group_id, user_id=int(bot.self_id))
+            if bot_info.get('role') not in ['owner', 'admin']:
+                return
+            await bot.set_group_ban(group_id=event.group_id, user_id=event.sender.user_id, duration=ban_duration)
+            await bot.send_group_msg(group_id=event.group_id, message=f'【{event.sender.nickname}】因为复读被禁言{ban_duration}分钟')
+            state['repeat_count'] = 0
+            state['last_message'] = ''
