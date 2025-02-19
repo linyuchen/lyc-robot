eb7da22f7b47fb06ef3568f6fa905103f0ec6e9e
linyuchen <lin.yu.chen@hotmail.com>
2025-02-19 19:52:05 +0800

    feat: plugin repeat check image

--- src/plugins/check_repeat.py
+++ src/plugins/check_repeat.py
@@ -1,6 +1,9 @@
+import asyncio
+import hashlib
 import random
 from collections import defaultdict
 
+import httpx
 from nonebot.plugin import PluginMetadata
 from nonebot import on_message
 from nonebot_plugin_alconna import UniMsg
@@ -17,7 +20,23 @@ group_message_state = defaultdict(lambda : {'repeat_count': 0, 'last_message': '
 @on_message().handle()
 async def _(bot: Bot, event: GroupMessageEvent):
     state = group_message_state[event.group_id]
-    current_message = event.message.extract_plain_text().strip()
+    current_message = ''
+    for msg in event.message:
+        if msg.type == 'image':
+            img_url = msg.data.get('url')
+            async with httpx.AsyncClient() as client:
+                response = await client.get(img_url)
+                hasher = hashlib.md5()
+                for chunk in response.iter_bytes(chunk_size=8192):
+                    # 在线程池中执行同步的hash.update操作
+                    await asyncio.get_running_loop().run_in_executor(
+                        None,
+                        hasher.update,
+                        chunk
+                    )
+                current_message += hasher.hexdigest()
+        elif msg.type == 'text':
+            current_message += msg.data.get('text')
     if not current_message:
         return
     if current_message == state['last_message']:
@@ -27,7 +46,7 @@ async def _(bot: Bot, event: GroupMessageEvent):
         if state['repeat_count'] >= repeat_max:
             state['repeat_count'] = 0
             state['last_message'] = ''
-            ban_duration = (repeat_count - 2)  * 60
+            ban_duration = (repeat_count - 2)  * 90
             bot_info = await bot.get_group_member_info(group_id=event.group_id, user_id=int(bot.self_id))
             if bot_info.get('role') not in ['owner', 'admin']:
                 return
