66cdce3ab680d839c65c1fb6daec8ffa0e51621e
linyuchen <lin.yu.chen@hotmail.com>
2025-11-05 15:47:07 +0800

    feat: add group album command for image referencing in group chats

---
+++ src/plugins/group_album.py
@@ -0,0 +1,29 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+from nonebot.internal.adapter import Message
+from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
+from nonebot_plugin_alconna import UniMsg
+
+__plugin_meta__ = PluginMetadata(
+    name="相册",
+    description="群相册功能，对着图片引用发送 相册 相册名",
+    usage="相册 相册1",
+)
+
+group_album_cmd = on_command('相册')
+
+
+@group_album_cmd.handle()
+async def _(bot: Bot, event: GroupMessageEvent, params: Message = CommandArg()):
+    if not params:
+        return
+        return await group_album_cmd.finish('请指定相册名，如 相册 相册名')
+    if not event.reply:
+        return
+        return await group_album_cmd.finish('请对图片引用发送此命令')
+    for reply_msg in event.reply.message:
+        if reply_msg.type == 'image':
+            img_url = reply_msg.data.get('url')
+            album_name = params.extract_plain_text().strip()
+            await bot.call_api('upload_group_album', group_id=event.group_id, album_name=album_name, file=img_url)
